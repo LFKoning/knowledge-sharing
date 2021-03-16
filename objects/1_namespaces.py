59147619f520a0847202f99c8ea65678fcbe7840
Lukas Koning <lfkoning@gmail.com>
2021-03-16 11:19:46 +0100

    Update hierarchy section

--- objects/1_namespaces.py
+++ objects/1_namespaces.py
@@ -1,34 +1,26 @@
-"""Demo namespaces."""
+"""Demo namespaces, run through debugger with breakpoints at #bp comments."""
 
 
 global_x = 1
 x = 1
 
 
-def function():
-
-    function_x = 3
-    x = 3
-
-    print(f"Function: global_x equals {global_x}.")
-    print(f"Function: x equals {x}.")
-
-    def inner():
-        print(f"Inner: global_x equals {global_x}.")
-        print(f"Inner: function_x equals {function_x}.")
-        print(f"Inner: x equals {x}.")
-
-    inner() #bp
-
-
 def main():
 
+    main_x = 2
     x = 2
 
     print(f"Main: global_x equals {global_x}.")
     print(f"Main: x equals {x}.")
 
-    function() #bp
+    def inner():
+        x = 3
+
+        print(f"Inner: global_x equals {global_x}.")
+        print(f"Inner: main_x equals {main_x}.")
+        print(f"Inner: x equals {x}.")
+
+    inner()  # breakpoint
 
 
 if __name__ == "__main__":
@@ -36,4 +28,5 @@ if __name__ == "__main__":
     print(f"Global: global_x equals {global_x}.")
     print(f"Global: x equals {x}.")
 
-    main() #bp
+    main()  # breakpoint
+
