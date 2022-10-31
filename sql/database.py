f3e1a5ff838550f776392fe5c7dae6408a4cedfa
Lukas Koning <lfkoning@gmail.com>
2022-10-31 15:50:19 +0100

    Updates to SQL exercises

--- sql/database.py
+++ sql/database.py
@@ -1,51 +1,58 @@
 """Module for the SQLite wrapper class."""
 import sqlite3
+from typing import Union
+
 import pandas as pd
 
 
 class Database:
-    """
-    SQLite Database wrapper with convenience functions.
-    
+    """Simple SQLite wrapper with some convenience functions.
+
     Parameters
     ----------
     filepath : str
         Path to the SQLite .db file.
     """
-    
-    def __init__(self, filepath):
+
+    def __init__(self, filepath: str) -> None:
         self._connection = sqlite3.connect(filepath)
-        
+
     def list_tables(self):
-        """List tables in the database."""
+        """List tables in the database.
+
+        Returns
+        -------
+        pandas.DataFrame
+            DataFrame listing the available tables.
+        """
 
         return self.query(
             "SELECT name, sql FROM sqlite_master WHERE type='table';"
         )
-        
-    def query(self, sql):
-        """
-        Execute the SQL statement against the database.
-        
+
+    def query(self, sql: str) -> Union[pd.DataFrame, int]:
+        """Execute the SQL statement against the database.
+
         Parameters
         ----------
         sql : str
             SQL query to perform
-        
+
         Returns
-        Union[pandas.DataFrame, int]
-            Pandas DataFrame for SELECT queries,
-            affected rows for all other queries.
+        -------
+        pandas.DataFrame or int
+            Pandas DataFrame for SELECT queries, affected rows for all
+            other query types.
         """
 
         with self._connection:
             cursor = self._connection.cursor()
             result = cursor.execute(sql)
-            
+
             # CREATE / INSERT / UPDATE queries
             if cursor.description is None:
                 return result.rowcount
-                          
+
             # SELECT queries
             else:
                 columns = [col[0] for col in cursor.description]
